6cba8a8602dfeee63aa54b676915f9a9918eec80
agrawaltejas01 <agrawaltejas01@gmail.com>
2022-01-01 13:36:07 +0530

    react-navigation init

--- App.js
+++ App.js
@@ -7,13 +7,21 @@
  */
 
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Home from './app/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-        <>
-            <Home />
-        </>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+        // <Home />
     );
 };
 
